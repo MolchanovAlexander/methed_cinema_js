3036622a42ddcc6ec6924d8548a16f203eba8e7a
Alexander Molchanov <rybkolhoz@gmail.com>
2021-12-08 21:17:58 +0200

    add API & renderVideo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import slideMenu from './menu.js'
-window.onload = function () {
-
-    slideMenu({
-        open_btn: '.header__burger-btn',
-        menu: '.navigation',
-        classActivMenu: 'navigation_active',
-        close_trigger: '.navigation__link, .navigation__close'
-    });
+import renderVideo from './renderVideo.js'
 
 
-}
+
+slideMenu({
+    open_btn: '.header__burger-btn',
+    menu: '.navigation',
+    classActivMenu: 'navigation_active',
+    close_trigger: '.navigation__link, .navigation__close'
+});
+
+renderVideo();
+
---
+++ js/services.js
@@ -0,0 +1,21 @@
+const API_KEY = '611b38bfc402f47502f495e8b2cf9a96';
+const BASE_URL ='https://api.themoviedb.org/3/';
+const LANGUAGE ='&language=ru-RU';
+
+
+//trending/all/day?api_key=<<api_key>>
+const getData = url =>{
+    return fetch(url).then( response =>{
+        if(response.ok){
+            return response.json()
+        }
+        throw `щось пішло не так ${response.status}`
+    })
+    .catch(err => console.log(err))
+}
+
+export const getTrands= async(type='all', period='week', page=1)=>{
+    const url =`${BASE_URL}/trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+    return await getData(url);
+    
+}
